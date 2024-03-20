a6e28f33624fb39ec07675b3313de28a29ee9ece
ReubenBaker <baker.reuben@outlook.com>
2024-03-20 08:10:29 +0000

    Improved roll view performance

--- Intervision/Views/Roll/Rows/BarRowsView.swift
+++ Intervision/Views/Roll/Rows/BarRowsView.swift
@@ -15,24 +15,24 @@ struct BarRowsView: View {
     let beats: Int
     
     var body: some View {
-        LazyVStack(spacing: 0) {
-            ForEach(0..<rows, id: \.self) { rowIndex in
-                ZStack {
+        ZStack {
+            LazyVStack(spacing: 0) {
+                ForEach(0..<rows, id: \.self) { rowIndex in
                     Rectangle()
                         .fill([1, 3, 5, 8, 10].contains(rowIndex % 12) ? Color.black.opacity(0.5) : Color.black.opacity(0.125))
                         .frame(height: rowHeight)
                         .border(Color.black.opacity(0.25))
-                    
-                    ForEach(0..<beats, id: \.self) { beatIndex in
-                        Path { path in
-                            let xPosition = CGFloat(beatIndex) * (rowWidth / CGFloat(beats))
-                            
-                            path.move(to: CGPoint(x: xPosition, y: 0))
-                            path.addLine(to: CGPoint(x: xPosition, y: rowHeight))
-                        }
-                        .stroke(Color.black.opacity(beatIndex == 0 ? 1.0 : 0.25))
-                    }
                 }
+            }
+            
+            ForEach(0..<beats, id: \.self) { beatIndex in
+                Path { path in
+                    let xPosition = CGFloat(beatIndex) * (rowWidth / CGFloat(beats))
+                    
+                    path.move(to: CGPoint(x: xPosition, y: 0))
+                    path.addLine(to: CGPoint(x: xPosition, y: rowHeight * CGFloat(rows)))
+                }
+                .stroke(Color.black.opacity(beatIndex == 0 ? 1.0 : 0.25))
             }
         }
     }
